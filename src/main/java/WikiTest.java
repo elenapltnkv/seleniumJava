f09b2cae95247fba40c16d53ad2314923edba7a5
Samelena <lyly-miranda@yandex.ru>
2021-10-30 18:30:08 +0300

    Initial commit

---
+++ src/main/java/WikiTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WikiTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://market.yandex.ru/");
+
+
+    }
+}
